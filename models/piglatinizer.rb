bef9f66b29cabce8010fca7797926d0d4701b7e4
Rachel Grier <rgrier104@gmail.com>
2019-08-14 22:11:26 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,10 +7,20 @@ class PigLatinizer
 
   def piglatinize_word(word)
     letter_array = word.split('')
-    first_letter = word[0].downcase
-    second_letter = word[1].downcase
-    third_letter = word[2].downcase
+    first3_array = word[0,3].split('')
+    sec1_array = []
+
     if word.scan(/[aeoui]/).count == 0
+      new_word = slice(1,word.length-1) + word[0] + "ay"
+    else
+      letter_array.map do |letter|
+        if letter not in 'aeoui'
+          sec1_array << letter
+        end
+      end
+    end
+  end
+
 
 
   def piglatin(text)
