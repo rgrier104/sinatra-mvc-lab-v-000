97306dfcc56274bc6dddf7e8b5de40532ca8aabf
Rachel Grier <rgrier104@gmail.com>
2019-08-14 22:36:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
       new_word = word + "way"
     else
       first3_array.map do |letter|
-        if letter.include?('aeoui')
+        if ('aeoui').include?(letter)
           sec1_array << letter
         end
       end
