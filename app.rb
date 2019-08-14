432fd91bb598015b8cd4db62f7e7a7b2e1bbe439
Rachel Grier <rgrier104@gmail.com>
2019-08-14 23:06:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analyzed_text = PigLatinizer.new(params[:user_text])
+    @analyzed_text = PigLatinizer.new(params[:user_phrase])
 
     erb :results
   end
