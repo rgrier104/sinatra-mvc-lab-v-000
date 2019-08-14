bba795bb51fda3a4848bfae439b67bd28746b468
Rachel Grier <rgrier104@gmail.com>
2019-08-14 21:35:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,14 @@ class PigLatinizer
     @text = text.downcase
   end
 
+  def piglatinize_word(word)
+    letter_array = word.split('')
+    first_letter = word[0].downcase
+    second_letter = word[1].downcase
+    third_letter = word[2].downcase
+    if word.scan(/[aeoui]/).count == 0
+      
+
   def piglatin(text)
     word_array = text.split(/\W+/)
     pig_array = word_array.map do |word|
